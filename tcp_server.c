f5f9d8076b67d26339a06e4f5cf123c2f9feb56e
Sofia Amallo <sofia@ideapad.fibertel.com.ar>
2022-03-23 00:06:18 -0300

    bind, listen, accept and send

--- tcp_server.c
+++ tcp_server.c
@@ -22,5 +22,20 @@ int main(){
     server_address.sin_port = htons(9002);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    
+    //bindeamos el socket a nuestro puerto ip especificado
+    bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+
+    listen(server_socket, 5); //5: cuantas conexiones puede esperar por este socket en un momento determinado
+
+    int client_socket;
+    client_socket = accept(server_socket, NULL, NULL);
+
+   //enviamos el mensaje
+    send(client_socket, server_message, sizeof(server_message), 0);
+
+    //cerrramos el servidor
+    close(server_socket);
+
+    return 0;
 }
