0c12784e59bacb5dd6e4452e8212d17b4334dcb3
Sofia Amallo <sofia@ideapad.fibertel.com.ar>
2022-03-22 23:54:38 -0300

    estructuras y socket de cliente

--- tcp_client.c
+++ tcp_client.c
@@ -10,3 +10,29 @@
 
 #define SIZE 256 //tamaño del buffer
 
+int main(){
+
+    //creo un socket
+    int network_socket;
+    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // especifico la dirección del socket
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002); //puerto probablemente libre
+    server_address.sin_addr.s_addr = INADDR_ANY; 
+
+    // connect(socket, estructura con la confuguracion del socket (hay que castearlo), tamaño de la estructura)
+    int connection_status = connect(network_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+
+|   // connect devuelve 0 en éxito, -1 en error
+    if(connection_status == -1){
+        printf("Hubo un error al hacer la conexión \n\n");
+    }
+
+    char server_response[SIZE];
+    
+
+
+
+}
